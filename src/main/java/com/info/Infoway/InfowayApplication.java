f0dbb7b420d1d511a76fada368bbdf1a55a18aae
varshadabade <varshadabade@gmail.com>
2019-06-14 16:10:57 +0530

    first commit

---
+++ src/main/java/com/info/Infoway/InfowayApplication.java
@@ -0,0 +1,13 @@
+package com.info.Infoway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfowayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfowayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/info/Infoway/InfowayApplicationTests.java
@@ -0,0 +1,11 @@
+package com.info.Infoway;
+
+import org.testng.annotations.Test;
+
+public class InfowayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
